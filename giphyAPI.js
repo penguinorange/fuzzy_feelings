2d726248ed5ff495bee6978d2fdabab87a1e36ea
Raymond Gordon <rjgordon26@gmail.comgit config --global user.email rjgordon26@gmail.com>
2020-10-07 13:45:01 -0500

    adding animal button function, modified tenor api query search

--- giphyAPI.js
+++ giphyAPI.js
@@ -32,6 +32,34 @@ $("#motive").on("click", function () {
     });
 });
 
+$("#animal").on("click", function () {
 
+    // Storing our giphy API URL for a random image
+    // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=3tLQ2TOR00NfbftDClkOtbHe3G2oTIy4";
+
+    var queryURL = "https://api.tenor.com/v1/search?key=XZCXO6QH7OGC&q=animal";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // After the data from the AJAX request comes back
+
+        var imageArr = response.results;
+        console.log(imageArr);
+
+        // Generate random index based on number of keys
+        var randIndex = Math.floor(Math.random() * imageArr.length);
+        console.log(randIndex);
+
+        var randImage = imageArr[randIndex].media[0].gif.url;
+        console.log(randImage);
+
+        var imageUrl = randImage;
+
+        $("#main-image").attr("src", imageUrl);
+    });
+});
 
 
