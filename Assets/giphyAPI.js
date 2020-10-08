d72f4b9658693d52e73fc6a9ca656da543b42fdd
jellyfish <johnrclifford@gmail.com>
2020-10-08 13:57:16 -0500

    added local storage function to setting, added name and value properties to input elements

--- Assets/giphyAPI.js
+++ Assets/giphyAPI.js
@@ -1,5 +1,44 @@
 
 
+//call reload on page load
+reload();
+
+//add click functionality to radio buttons
+Array.from(document.querySelectorAll('input[type="radio"]')).forEach(function(item, index) {
+  item.addEventListener('click', save);
+});
+
+//Save Values
+function save() {
+  //Radiobuttons  
+  var g1 = document.querySelector('input[name=radio]:checked');
+  console.log(this)
+  // the line below is an if else statement --> look up "ternary"
+// g1 = (g1) ? g1.value : '';   <------ this line of code is the same as the 5 lines immediately below
+  if (g1)   {
+      g1 = g1.value
+  } else {
+      g1 = "";
+  }
+  console.log(g1)
+  localStorage.setItem("g1", g1);
+}
+//Restoring saved Values
+function reload() {
+  // Radiobuttons    
+  // get a list of DOM elements
+  var G1 = Array.from(document.getElementsByName('radio'));
+  var val1 = localStorage.getItem('g1');
+  for (var i = 0; i < G1.length; i++) {
+    if (G1[i].value == val1) {
+      G1[i].checked = true;
+    }
+  }
+}
+
+
+
+
 
 // Event listener for our motive-button
 $("#motive").on("click", function () {
